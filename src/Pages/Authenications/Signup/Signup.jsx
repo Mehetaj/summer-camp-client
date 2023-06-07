c9d11849420966855e98e11575b716358b93c770
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-06-07 13:16:50 +0600

    banner added

--- src/Pages/Authenications/Signup/Signup.jsx
+++ src/Pages/Authenications/Signup/Signup.jsx
@@ -1,33 +1,52 @@
 import React, { useState } from 'react';
 import bg from '../../../assets/Login.jpg'
 import { Link, useNavigate } from 'react-router-dom';
-import {  useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import useAuth from '../../../Hooks/useAuth';
+const img_hosting_token = import.meta.env.VITE_image_upload_token
 
 const Signup = () => {
     const [error, setError] = useState("");
     const { createUser, updateUserProfile } = useAuth()
     const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
 
     const onSubmit = (data) => {
         console.log(data);
-        const { name, email, password, confirmpassword } = data;
+        const { name, email, password, confirmpassword, photo } = data;
         if (password !== confirmpassword) {
             setError("Password Not Matched");
             return;
         };
+        const formData = new FormData();
+        formData.append('image', data.photo)
 
-        createUser(email, password)
-            .then(result => {
-                console.log(result.user);
-                if (result.user) {
-                    updateUserProfile(name)
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgRes => {
+                console.log(imgRes);
+                if (imgRes.success) {
+                    const photo = imgRes.data.display_url;
+                    createUser(email, password)
+                        .then(result => {
+                            console.log(result.user);
+                            if (result.user) {
+                                updateUserProfile(name, photo)
+                            }
+                            setError("")
+                            navigate('/')
+
+                        })
                 }
-                setError("")
-                navigate('/')
-                
             })
+
+
+
+
     }
     return (
         <div className='flex justify-around items-center'>
@@ -61,12 +80,16 @@ const Signup = () => {
                                 message: 'Password must contain a capital letter and a special character',
                             },
                         })} type="password" className="block py-2.5 px-0 w-full text-sm text-rose-900 bg-transparent border-0 border-b-2 border-rose-300 appearance-none dark:text-white dark:border-rose-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
-                        
+
                         <label className="peer-focus:font-medium absolute text-sm text-rose-500 dark:text-rose-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
                         <input {...register("confirmpassword", { required: true })} type="password" className="block py-2.5 px-0 w-full text-sm text-rose-900 bg-transparent border-0 border-b-2 border-rose-300 appearance-none dark:text-white dark:border-rose-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
                         <label className="peer-focus:font-medium absolute text-sm text-rose-500 dark:text-rose-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm Password</label>
+                    </div>
+                    <div className="relative mt-10 z-0 w-full group">
+                        <input type="file" className="file-input mb-6 file-input-bordered file-input-secondary mt-3 w-full max-w-xs" />
+                        <label className="peer-focus:font-medium block absolute text-sm text-rose-500 dark:text-rose-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-600  peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
                     </div>
                     <input className='btnp w-full my-4' type="submit" value="Login" />
                     <p>Already Have an Account? <Link to="/login" className='text-red-400 font-semibold'>Login Here</Link></p>
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import img1 from '../../../assets/img1.jpg'
+import img2 from '../../../assets/img2.jpg'
+import img3 from '../../../assets/img3.jpg'
+import img4 from '../../../assets/img4.jpg'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
+const Banner = () => {
+    return (
+        <div>
+            <Swiper
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={img1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img3} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img4} alt="" />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Banner;
