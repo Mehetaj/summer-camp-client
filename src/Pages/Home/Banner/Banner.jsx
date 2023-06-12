37177ec037aac25018a0fa2bc48d935892d7ea3a
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-06-12 17:34:04 +0600

    react anime added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import img2 from '../../../assets/img1.jpg'
 import img1 from '../../../assets/Untitled+design+(1).png'
 import img3 from '../../../assets/m72951594113_1.webp'
@@ -20,16 +20,16 @@ const Banner = () => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img className='w-full h-[900px]' src={img1} alt="" />
+                    <img className='w-full lg:w-[1550px]  h-[400px] lg:h-[900px]' src={img1} alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='w-full h-[900px]' src={img2} alt="" />
+                    <img className='w-full lg:w-[1550px]  h-[400px] lg:h-[900px]' src={img2} alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='w-full h-[900px]' src={img3} alt="" />
+                    <img className='w-full lg:w-[1550px]  h-[400px] lg:h-[900px]' src={img3} alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='w-full h-[900px]' src={img4} alt="" />
+                    <img className='w-full lg:w-[1550px]  h-[400px] lg:h-[900px]' src={img4} alt="" />
                 </SwiperSlide>
             </Swiper>
         </div>
--- src/Pages/Home/ClassDemo/OneClassDemo.jsx
+++ src/Pages/Home/ClassDemo/OneClassDemo.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import anime from 'animejs/lib/anime.es.js';
 
-const OneClassDemo = ({classes}) => {
-    // console.log(classes);
-    const {photo,name,instructor, email} = classes
+const element = document.getElementById('elem');
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
     return (
-        <div className='border w-[400px] border-red-500 rounded-xl'>
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Function to handle the scroll event
+  function handleScroll() {
+    if (isInViewport(box)) {
+      anime({
+        targets: element,
+        opacity: 1,
+        translateY: ['50px', '0px'],
+        easing: 'easeOutExpo',
+        duration: 1000
+      });
+  
+      // Remove the scroll event listener once the animation has been triggered
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }
+
+const OneClassDemo = ({ classes }) => {
+    // console.log(classes);
+    const { photo, name, instructor, email } = classes
+    return (
+        <div id='elem' className='border w-[400px] border-red-500 rounded-xl'>
             <img className='w-[400px] h-[300px] rounded-t-xl' src={photo} alt="" />
             <div className='p-6'>
                 <p className='text-3xl font-bold my-4'>{name}</p>
 
-            <p className='text-xl font-semibold mt-3'>Instructor: {instructor}</p>
-            <p className='mt-3'>Email Address : {email}</p>
+                <p className='text-xl font-semibold mt-3'>Instructor: {instructor}</p>
+                <p className='mt-3'>Email Address : {email}</p>
             </div>
 
         </div>
